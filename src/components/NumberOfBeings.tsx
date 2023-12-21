3fce9d8c18460693e8acb2bbf5b5ccd0000e9940
James Tang <133535682+environmentallyconsciouscoder@users.noreply.github.com>
2023-12-21 03:47:14 +0000

    feat: added forms validator

--- src/components/NumberOfBeings.tsx
+++ src/components/NumberOfBeings.tsx
@@ -1,20 +1,32 @@
 import React, { ChangeEvent } from "react";
+import ErrorMessage from "./ErrorMessage";
 
 type Props = {
   numberOfBeings: string;
   onChangeNumberOfBeings: (value: string) => void;
+  validate: (value: string) => Array<string>;
 };
 
 
-const NumberOfBeings: React.FC<Props> = ({numberOfBeings, onChangeNumberOfBeings}: Props) => {
+const NumberOfBeings: React.FC<Props> = ({numberOfBeings, onChangeNumberOfBeings, validate}: Props) => {
+
+  const errorMessage = validate(numberOfBeings);
+  const isErrorMessageDefined = validate(numberOfBeings) !== undefined ? errorMessage : [];
+
   return (
-    <input
-      id='numberOfBeings'
-      data-testid="numberOfBeings"
-      type='number'
-      value={numberOfBeings}
-      onChange={(e) => onChangeNumberOfBeings(e.target.value)}
-    />
+    <>
+      <input
+        id='numberOfBeings'
+        data-testid="numberOfBeings"
+        type='number'
+        value={numberOfBeings}
+        onChange={(e) => onChangeNumberOfBeings(e.target.value)}
+        min="1000000000" // Set the minimum value
+      />
+      { isErrorMessageDefined.length !== 0 &&
+        <ErrorMessage errorMessage={isErrorMessageDefined}/>
+      }
+    </>
   );
 };
 
--- src/components/PlanetName.tsx
+++ src/components/PlanetName.tsx
@@ -1,12 +1,19 @@
 import React from "react";
+import ErrorMessage from "./ErrorMessage";
 
 type Props = {
     planetName: string;
     onChangePlanetName: (value: string) => void;
+    validate: (value: string) => Array<string>;
   };
 
-const PlanetName: React.FC<Props> = ({planetName, onChangePlanetName}: Props) => {
+const PlanetName: React.FC<Props> = ({planetName, onChangePlanetName, validate}: Props) => {
+
+  const errorMessage = validate(planetName);
+  const isErrorMessageDefined = validate(planetName) !== undefined ? errorMessage : [];
+
   return (
+    <>
     <input
       id='planetName'
       data-testid="planetName"
@@ -14,6 +21,10 @@ const PlanetName: React.FC<Props> = ({planetName, onChangePlanetName}: Props) =>
       value={planetName}
       onChange={(e) => onChangePlanetName(e.target.value)}
     />
+    { isErrorMessageDefined.length !== 0 &&
+      <ErrorMessage errorMessage={isErrorMessageDefined}/>
+    }
+    </>
   );
 };
 
