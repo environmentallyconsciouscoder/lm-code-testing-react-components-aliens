8835f54277ce2921bec6da59d512f844a38a43fc
James Tang <133535682+environmentallyconsciouscoder@users.noreply.github.com>
2023-12-21 03:45:05 +0000

    test: added test to validate the error message

--- src/components/PlanetName.test.tsx
+++ src/components/PlanetName.test.tsx
@@ -1,12 +1,24 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import PlanetName from './PlanetName';
+const mockOnChange = jest.fn();
+const mockValidator = jest.fn();
 
 test('Given the props, when the component is rendered, then the state should change', () => {
-    const mockOnChange = jest.fn();
-    render(<PlanetName planetName="" onChangePlanetName={mockOnChange} />);
+
+    render(<PlanetName planetName="" onChangePlanetName={mockOnChange} validate={mockValidator} />);
 
     const inputElement = screen.getByTestId("planetName");
     fireEvent.change(inputElement, { target: { value: "earth" } });
 
     expect(mockOnChange).toHaveBeenCalledWith("earth"); // Check if the function was called with the correct value
 });
+
+test('Given the props, when the component is rendered, then the screen would show text Must be between 2 and 49 characters, and No special characters allowed!', () => {
+
+    mockValidator.mockReturnValue(["Must be between 2 and 49 characters", "No special characters allowed!"]);
+
+    render(<PlanetName planetName="" onChangePlanetName={mockOnChange} validate={mockValidator} />);
+
+    expect(screen.getByText("Must be between 2 and 49 characters")).toBeInTheDocument();
+    expect(screen.getByText("No special characters allowed!")).toBeInTheDocument();
+})
